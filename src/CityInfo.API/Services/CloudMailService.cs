20db4b27b96068006a256c03a8ffda4d32f7633b
Tomasz Matuszczak <tomasz.matuszczak@gmail.com>
2017-04-10 14:40:42 +0200

    Configuration files added.

--- src/CityInfo.API/Services/CloudMailService.cs
+++ src/CityInfo.API/Services/CloudMailService.cs
@@ -4,8 +4,8 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "tm.forward1@gmail.com";
-        private string _mailFrom = "tm.forward1@gmail.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
         public void Send(string subject, string message)
         {
